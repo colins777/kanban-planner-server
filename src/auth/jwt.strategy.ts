7a06f187b5b3e13d3ec7ac7c519499736fcfebe1
colins777 <rumyga@gmail.com>
2024-03-07 00:01:15 +0200

    added auth/user controllers

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { validate } from "class-validator";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserService } from "src/user/user.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private configService: ConfigService,
+        private userService: UserService
+    ) {
+            super({
+                //BearerToken is in header request
+                jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+                ignoreExpiration: true,
+                secretOrKey: configService.get('JWT_SECRET')
+            })
+    }
+
+//set user_id in token, only we can encode this using JWT_SECRET
+async validate({id}: {id: string}) {
+    return this.userService.getById(id)
+}
+
+}
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  //add /api to every request
+  app.setGlobalPrefix('api')
+  app.use(cookieParser())
+  app.enableCors({
+    //port of front end part - client
+    origin: ['http://localhost:4200'],
+    credentials: true,
+    //not required
+    exposedHeaders: 'set-cookie'
+
+  })
+
+ // await app.listen(3000);
+ await app.listen(4200);
 }
 bootstrap();

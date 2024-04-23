c1d4e4954a13de64699b712a5e63cb3252fd6ea3
colins777 <rumyga@gmail.com>
2024-04-23 23:56:24 +0300

    refresh token f-ty added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import {TaskModule} from "./task/task.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    AuthModule, 
-    UserModule
+    AuthModule,
+    UserModule,
+    TaskModule
   ],
 
 })
